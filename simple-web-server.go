9be0ad7c9e2dd436e27ce841a4b0edf53424f915
Kostis Kapelonis <kostis@codefresh.io>
2022-03-11 17:57:36 +0200

    Added extra properties

--- simple-web-server.go
+++ simple-web-server.go
@@ -7,14 +7,19 @@ import (
 )
 
 type configurationListHandler struct {
-	version          string
-	environment      string
-	environment_type string
-	region           string
-	paypal_url       string
-	db_user          string
-	db_password      string
-	gpu_enabled      string
+	version           string
+	environment       string
+	environment_type  string
+	region            string
+	paypal_url        string
+	db_user           string
+	db_password       string
+	gpu_enabled       string
+	ui_theme          string
+	cache_size        string
+	page_limit        string
+	sorting           string
+	number_of_buckets string
 }
 
 func (h *configurationListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +33,11 @@ func (h *configurationListHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 	fmt.Fprintf(w, "<li>db_user: "+h.db_user+"</li>")
 	fmt.Fprintf(w, "<li>db_password: "+h.db_password+"</li>")
 	fmt.Fprintf(w, "<li>gpu_enabled: "+h.gpu_enabled+"</li>")
+	fmt.Fprintf(w, "<li>ui_theme: "+h.ui_theme+"</li>")
+	fmt.Fprintf(w, "<li>cache_size: "+h.cache_size+"</li>")
+	fmt.Fprintf(w, "<li>page_limit: "+h.page_limit+"</li>")
+	fmt.Fprintf(w, "<li>sorting: "+h.sorting+"</li>")
+	fmt.Fprintf(w, "<li>number_of_buckets: "+h.number_of_buckets+"</li>")
 	fmt.Fprintf(w, "</ol>")
 
 }
@@ -39,13 +49,6 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	// cfg, err := ini.LooseLoad("/config/settings.ini", "settings.ini")
-	// if err != nil {
-	// 	fmt.Printf("Fail to read configuration file: %v", err)
-	// }
-
-	// fmt.Println("App Mode:", cfg.Section("").Key("app_mode").String())
-
 	clh := configurationListHandler{}
 	clh.version = "3.0"
 	clh.environment = os.Getenv("ENV")
@@ -55,6 +58,11 @@ func main() {
 	clh.db_user = os.Getenv("DB_USER")
 	clh.db_password = os.Getenv("DB_PASSWORD")
 	clh.gpu_enabled = os.Getenv("GPU_ENABLED")
+	clh.ui_theme = os.Getenv("UI_THEME")
+	clh.cache_size = os.Getenv("CACHE_SIZE")
+	clh.page_limit = os.Getenv("PAGE_LIMIT")
+	clh.sorting = os.Getenv("SORTING")
+	clh.number_of_buckets = os.Getenv("N_BUCKETS")
 
 	fmt.Println("Simple web server is starting on port 8080...")
 	http.Handle("/", &clh)
